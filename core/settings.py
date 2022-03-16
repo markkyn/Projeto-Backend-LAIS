c562a06684bd3347ac0632b6d893cf4bbe7fd47b
unknown <marcos.gabriel@ossirian.com>
2022-03-16 02:46:21 -0300

    Some Bootstrap and Styles added to the Templates (Via CDN)

--- core/settings.py
+++ core/settings.py
@@ -107,6 +107,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
